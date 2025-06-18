f5e1fb7ee022230b449028395e2fe8bf42196313
GURUPRASAD H D <gurudilp456@gmail.com>
2025-06-18 22:21:58 +0530

    Rebuilding

--- tips/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Waiter
-
-class WaiterForm(forms.ModelForm):
-    class Meta:
-        model = Waiter
-        fields = ['name', 'photo', 'payment_link']
